d495ddcbb9471f04f4df4080ceaea47e0b7f6b4f
Youngjae Choi <cdw0807@naver.com>
2021-11-25 23:42:17 +0900

    commit3

--- src/P5_소수만들기.java
+++ src/P5_소수만들기.java
@@ -1,8 +1,8 @@
 public class P5_소수만들기 {
     
     public static int solution(int[] nums) {
-
-
+        return 1;
+    }
     public static void main(String[] args) {
         int[] nums = {1, 2, 7, 6, 4};
 
---
+++ src/P6_완주하지못한선수.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class P6_완주하지못한선수 {
+
+    public static String solution(String[] participant, String completion[]) {
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+
+        int i;
+
+        for (i = 0; i < completion.length; i++) {
+            if (!participant[i].equals(completion[i]))
+                break;
+            }
+
+        return participant[i];
+    }
+
+    public static void main(String[] arg) {
+        String[] a = {"loe", "kiki", "eden", "kiki"};
+        String[] b = {"eden", "kiki", };
+
+        System.out.println(solution(a, b));
+    }
+}
