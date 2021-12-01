b951d361ead283430e465dc61db21d5ef452c90c
Youngjae Choi <cdw0807@naver.com>
2021-12-01 17:55:56 +0900

    commit5

---
+++ src/P14_예산.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class P14_예산 {
+
+    public static int solution(int[] d, int budget) {
+        int answer = 0;
+        int sum = 0;
+
+        Arrays.sort(d);
+
+        for (int i = 0; i < d.length; i++) {
+            sum += d[i];
+            
+            if (sum > budget) {
+                answer = i;
+                break;
+            }
+            else if (sum <= budget) answer = d.length;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] d = {1, 3, 2, 5, 4};
+        int budget = 9;
+
+        System.out.println(solution(d, budget));
+    }
+    
+}
---
+++ src/P15_두개뽑아서더하기.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class P15_두개뽑아서더하기 {
+
+    public static int[] solution(int[] numbers) {
+        ArrayList<Integer> result = new ArrayList<>();
+
+        for (int i = 0; i < numbers.length - 1; i++) {
+            for(int j = i+1; j < numbers.length; j++) {
+                int sum = numbers[i] + numbers[j];
+                if (result.contains(sum)) continue;
+                else result.add(sum);
+            }
+        }
+
+        int[] answer = new int[result.size()];
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = result.get(i);
+        }
+
+        Arrays.sort(answer);
+    
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {2, 1, 3, 4, 1};
+        System.out.println(Arrays.toString(solution(numbers)));
+    }
+    
+}
