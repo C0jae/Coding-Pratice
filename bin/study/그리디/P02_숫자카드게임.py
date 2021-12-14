2471bc93ad4b969ac64fbf222705615d17b29796
Youngjae Choi <cdw0807@naver.com>
2021-12-14 13:41:57 +0900

    21.12.14

--- bin/study/그리디/P02_숫자카드게임.py
+++ bin/study/그리디/P02_숫자카드게임.py
@@ -1,8 +1,10 @@
 #n, m값 입력
 n, m = map(int, input("n, m값 입력 : ").split())
 
+# 결과값 변수 선언 및 정의
 result = 0
 
+# 배열 생성후 최소값 산출 및 result와 비교하여 최대값 산출 및 정의 -> n번만큼 반복
 for i in range(n):
     arr = list(map(int, input("카드값 입력 : ").split()))
     min_value = min(arr)
---
+++ bin/study/그리디/P03_1이될때까지.py
@@ -0,0 +1,20 @@
+# n, k값 입력
+n, k = map(int, input("n, k값 입력 : ").split())
+
+# 실행횟수 변수 선언
+cnt = 0;
+
+# n값이 1이 될때까지 무한반복
+while n != 1:
+    
+    # n이 k의 배수가 아니라면 n = n - 1 진행 및 cnt +1
+    if (n % k != 0):
+        n -= 1
+        cnt += 1
+    
+    # n이 k의 배수일 경우 n = n / k 진행 및 cnt +1
+    else:
+        n /= k
+        cnt += 1
+        
+print(cnt)
