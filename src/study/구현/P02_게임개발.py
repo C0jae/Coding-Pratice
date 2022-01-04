ae50cfefdb178a3b77825f5d0fb4137725979adc
Youngjae Choi <cdw0807@naver.com>
2022-01-04 17:05:55 +0900

    22.01.04

---
+++ src/study/구현/P02_게임개발.py
@@ -0,0 +1,80 @@
+# def check(x, y):
+
+# 방문한 칸의 수    
+cnt = 0
+
+# 방향을 돌린 횟수
+time = 0
+
+n, m = map(int, input("행, 열 입력 : ").split())
+a, b, d = map(int, input("캐릭터 위치 및 방향 표시 : ").split())
+
+# 나침반(북, 동, 남, 서)
+cd = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+
+data = [[0] * m for i in range(n)]
+
+for i in range(n):
+    data[i] = list(map(int,input("행 바다 or 육지 선택 : ").split()))
+        
+boolean = True
+while(boolean):
+    # 움직이려는 방향이 d 범위안에 있을경우
+    if (a + cd[d][0] >= 0 and a + cd[d][0] < n and 
+        b + cd[d][1] >= 0 and b + cd[d][1] < m):
+        
+        print("======================")
+        print("카운트 : ", cnt)
+        print("방향 : ", d)
+        print("땅 : " ,data[a + cd[d][0]][b + cd[d][1]])
+        print("time : ", time)
+        
+        # 움직이려는 방향이 육지인경우
+        if (data[a + cd[d][0]][b + cd[d][1]] == 0):
+            time = 0
+            print("이동 좌표1 : ", a, b)
+            data[a][b] = 2
+            a += cd[d][0]
+            b += cd[d][1]
+            print("이동 좌표2 : ", a, b)
+            cnt += 1
+            
+        # 움직이려는 방향이 바다인경우
+        else:
+            # 방향전환
+            if (d == 0): d = 3
+            elif (d == 1): d = 0
+            elif (d == 2): d = 1
+            elif (d == 3): d = 2
+            time += 1
+            
+            # 4방향 모두 가본칸이거나 바다일 경우 + 뒤로 이동 가능할 경우
+            if (time == 6 and data[a + cd[d][0]][b + cd[d][1]] == 2):
+                data[a][b] = 3
+                a += cd[d][0]
+                b += cd[d][1]
+                cnt += 1
+                time = 0
+                print("=======뒤로이동=======")
+                print("이동좌표 : ", a, b)
+                print("======================")
+                
+                continue
+            
+            # 4방향 모두 가본칸이거나 바다일 경우 + 뒤로 이동 불가능할 경우 종료
+            elif (time == 6 and data[a + cd[d][0]][b + cd[d][1]] != 2):
+                break
+        
+            continue
+            
+    else:
+        # 방향전환
+            if (d == 0): d = 3
+            elif (d == 1): d = 2
+            elif (d == 2): d = 1
+            elif (d == 3): d = 0
+            time += 1
+            
+            print(cnt)
+        
+print(cnt)
---
+++ src/study/그리디/P07_만들수없는금액.py
@@ -0,0 +1,18 @@
+n = int(input("동전 갯수 입력 : "))
+coin = list(map(int, input("화폐단위 입력 : ").split()))
+
+# 결과값 result 1로 정의
+# 가장 작은 화폐가 2원 이상일 경우 for문을 바로 빠져나와 1의 값을 출력하기 위해
+result = 1
+
+# 화폐단위 오름차순 정렬
+coin.sort()
+
+for i in coin:
+    if i > result:
+        break
+    
+    result += i
+
+print(result)
+        
