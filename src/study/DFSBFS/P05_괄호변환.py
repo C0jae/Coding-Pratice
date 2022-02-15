9b4b1b393e1d0c232a390a890dab2b5e3140ed98
Youngjae Choi <cdw0807@naver.com>
2022-02-15 20:51:39 +0900

    22.02.15

--- src/study/DFSBFS/P05_괄호변환.py
+++ src/study/DFSBFS/P05_괄호변환.py
@@ -1,3 +1,4 @@
+# 균형있는 코드여부 검토 -> 균형있지 않았다면 균형있던 곳 까지의 인덱스 리턴
 def balaced_index(p):
     count = 0
     
@@ -11,6 +12,7 @@ def balaced_index(p):
         if count == 0:
             return i
 
+# 올바른 문자여부 검토
 def check_proper(p):
     count = 0
     for i in p:
@@ -18,24 +20,32 @@ def check_proper(p):
             count += 1
         
         else:
+            # 남아있는 '('가 없이 ')'가 올 경우 올바른 문자x
             if count == 0:
                 return False
             
             count -= 1
     
+    # 올바른 문자로 판별되면 True 리턴
     return True
 
 
 def solution(p):
     answer =''
     
+    # 아무런 단어도 없다면 빈 answer 리턴
     if p == '':
         return answer
     
+    # 균형있는 곳까지의 인덱스 값 가져오기
     index = balaced_index(p)
+    # 균형있는 곳까지의 값 u
     u = p[:index + 1]
+    
+    # 균형있지 않은 곳부터의 값 v
     v = p[index + 1:]
     
+    # 균형있는 u값이 제대로 배열도 되어있을경우 answer에 u값 적용 및 v값만으로 다시 반복
     if check_proper(u):
         answer = u + solution(v)
     
