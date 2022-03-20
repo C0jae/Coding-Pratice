ee65e9a79655fbfbb5793de7f1a6cbd2a471f6d8
Youngjae Choi <cdw0807@naver.com>
2022-03-20 18:23:52 +0900

    22.03.20

--- src/note/pythonNote.py
+++ src/note/pythonNote.py
@@ -2,9 +2,3 @@ a = "ag?#"
 
 c = a.count("?")
 queries = ["fro?w", "????o", "fr???", "fro???", "pro?"]
-
-print(a[1:])
-print(queries[0][:1])
-
-print(queries[0][-1])
-print(queries[0][:2 + 1])
--- src/study/최단경로/P04_정확한순위.py
+++ src/study/최단경로/P04_정확한순위.py
@@ -5,13 +5,48 @@ n, m = map(int, input("학생수, 비교할 횟수 : ").split())
 
 q = deque()
 
+# a, b값 입력받으면서 q에 데이터 입력
 for _ in range(m):
     a, b = map(int, input("A, B 값 입력 : ").split())
     q.append((a, b))
 
-INF = int(1e9)
+# 길이 n + 1인 리스트 생성
 data = [[0] * (n + 1) for _ in range(n + 1)]
 
+# 같은 행렬(같은학생)인 곳은 2로 표시
+for i in range(1, len(data)):
+    for j in range(1, len(data)):
+        if i == j:
+            data[i][j] = 2
+
+# 입력된 성적비교 데이터 하나하나 적용
 while q:
     a, b = q.popleft()
     
+    data[a][b] = 1  # a학생이 b학생보다 성적이 낮다 => 1 입력
+    data[b][a] = -1 # b학생이 a학생보다 성적이 높다 => -1 입력
+    
+    for i in range(1, len(data)):
+        # b학생보다 성적이 높은 학생이 있는경우
+        if data[b][i] == 1:
+            data[a][i] == 1 # a학생보다도 점수가 높다
+            data[i][a] == -1
+        
+        # a학생보다 성적이 낮은 학생이 있는경우
+        if data[i][a] == 1:
+            data[i][b] = 1  # b학생보다도 점수가 낮다
+            data[b][i] = -1
+        
+result = 0
+# 각 행을 조사하여(각 학생을 조사하여) 해당 행에 0이 없다면
+# (다른 학생과의 높고 낮음의 정보가 있다 => 순위를 알 수 있음) result +1
+for i in range(1, len(data)):
+    cnt = 0
+    for j in range(1, len(data)):
+        if data[i][j] != 0:
+            cnt += 1
+
+    if cnt == n:
+        result += 1
+    
+print(result)   # 결과출력
