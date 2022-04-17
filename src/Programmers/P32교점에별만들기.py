81728fb650da40fd112a2cf05bd68136401019bb
Youngjae Choi <cdw0807@naver.com>
2022-04-17 16:58:54 +0900

    22.04.17

--- src/Programmers/P32교점에별만들기.py
+++ src/Programmers/P32교점에별만들기.py
@@ -3,35 +3,43 @@ def solution(line):
     crossX = [] # 교점 x좌표
     crossY = [] # 교점 y좌표
     
+    # 두 직선의 교점 구하기
     for i in range(len(line) - 1):
         for j in range(i + 1, len(line)):
+            # 직선1
             a = line[i][0]
             b = line[i][1]
             e = line[i][2]
             
+            직선2
             c = line[j][0]
             d = line[j][1]
             f = line[j][2]
             
+            if a * d == b * c:
+                continue
+            
+            x = ((b * f) - (e * d)) / ((a * d) - (b * c))
+            y = ((e * c) - (a * f)) / ((a * d) - (b * c))
+            
             # 교점이 정수라면 cross 리스트에 추가하기
-            if (a * d) != (b * c) and ((b * f) - (e * d)) % ((a * d) - (b * c)) == 0 and ((e * c) - (a * f)) % ((a * d) - (b * c)) == 0:
-                x = int(((b * f) - (e * d)) / ((a * d) - (b * c)))
-                y = int(((e * c) - (a * f)) / ((a * d) - (b * c)))
-                
-                cross.append((x, y))
-                crossX.append(x)
-                crossY.append(y)
+            if x == int(x) and y == int(y):
+                cross.append((int(x), int(y)))
+                crossX.append(int(x))
+                crossY.append(int(y))
     
-    minX = min(crossX) if min(crossX) >= 0 else (-1 * min(crossX))
-    minY = min(crossY) if min(crossY) >= 0 else (-1 * min(crossY))
+    minX = min(crossX)
+    maxX = max(crossX)
+    minY = min(crossY)
+    maxY = max(crossY)
     
-    l = len(crossX)
-    answer = [["."] * (max(crossX) - min(crossX) + 1) for _ in range((max(crossY) - min(crossY) + 1))]
+    answer = ['.' * (maxX - minX + 1)] * (maxY - minY + 1)
     
-    for i in range(l):
-        answer[-crossY[i] + minY][crossX[i] + minX] = "*"
-                
-    return ["".join(ans) for ans in answer]
+    for c in cross:
+        x, y = c
+        answer[maxY - y] = answer[maxY - y][:x - minX] + '*' + answer[maxY - y][x - minX + 1:]
+    
+    return answer
 
 line = [[2, -1, 4], [-2, -1, 4], [0, -1, 1], [5, -8, -12], [5, 8, 12]]
 
