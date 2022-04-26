c87c1cdf38dc76a208da4210d7c2880863ac92fd
Youngjae Choi <cdw0807@naver.com>
2022-04-26 22:02:06 +0900

    22.04.26

--- src/Programmers/P35_전력망을둘로나누기.py
+++ src/Programmers/P35_전력망을둘로나누기.py
@@ -13,8 +13,9 @@ def union_parent(a, b, parent):
         parent[b] = a
     else:
         parent[a] = b
-
+    
 def solution(n, wires):
+    wires.sort()
     result = []
     
     # 경로를 하나씩 끊어보기 위한 반복문
@@ -22,7 +23,7 @@ def solution(n, wires):
         parent = [0] * (n + 1)
         for k in range(n + 1):
             parent[k] = k
-
+        
         # 하나의 경로는 무시하기(= 끊기)
         for j in range(len(wires)):
             if i == j:
@@ -39,7 +40,10 @@ def solution(n, wires):
     # print(result)
     return min(result)
 
-n = 9
-wires = [[1,3],[2,3],[3,4],[4,5],[4,6],[4,7],[7,8],[7,9]]
+n = 6
+wires = [[1, 4], [3, 6], [2, 5], [1, 5], [3, 5]]
+# n = 9
+# wires = [[1,3],[2,3],[3,4],[4,5],[4,6],[4,7],[7,8],[7,9]]
 
+print(sorted(wires))
 print(solution(n, wires))
